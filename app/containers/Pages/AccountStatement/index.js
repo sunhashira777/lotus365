9c0e2ec88a5a4e6dde26c2c928804a3df8f6d202
unknown <madhavatechsolutions@gmail.com>
2026-03-27 23:41:53 +0530

    feat: account statement and wallet balance

--- app/containers/Pages/AccountStatement/index.js
+++ app/containers/Pages/AccountStatement/index.js
@@ -14,214 +14,172 @@ import { useSelector } from 'react-redux';
 
 function AccountStatement() {
   const startDatePickerRef = useRef(null);
-  const [sportFilter, setSportFilter] = useState('');
   const endDatePickerRef = useRef(null);
+
+  const [sportFilter, setSportFilter] = useState('');
   const [startDate, setStartDate] = useState(() => {
     const today = new Date();
     today.setDate(today.getDate() - 2);
     return today;
   });
-  const { id } = useSelector((state) => state.user || { id: null });
   const [endDate, setEndDate] = useState(new Date());
+
   const [statementData, setStatementData] = useState([]);
   const [page, setPage] = useState(1);
-  const take = 15;
+  const take = 10;
+
   const [pagination, setPagination] = useState({
     totalCount: 0,
   });
 
   useEffect(() => {
-    if (id !== undefined && startDate && endDate) {
+    if (startDate && endDate) {
       getTransactionList();
     }
-  }, [id, page, take, startDate, endDate, sportFilter]);
+  }, [page, startDate, endDate, sportFilter]);
 
   const getTransactionList = async () => {
-    const islogin = isLoggedIn();
-    if (islogin) {
-      try {
-        const params = getQueryString({
-          // filterUserId: id,
-          offset: (page - 1) * take,
-          limit: take,
-          startDate: moment(startDate).startOf('day').toISOString(),
-          endDate: moment(endDate).endOf('day').toISOString(),
-          sportid:
-            sportFilter === 'cricket'
-              ? 4
-              : sportFilter === 'soccer'
-              ? 1
-              : sportFilter === 'tennis'
-              ? 2
-              : '',
-          filterTransaction:
-            sportFilter === 'settling'
-              ? 'settling'
-              : ['cricket', 'soccer', 'tennis'].includes(sportFilter)
-              ? 'betting'
-              : '',
+    if (!isLoggedIn()) return;
+
+    try {
+      const params = getQueryString({
+        page,
+        limit: take,
+        fromDate: moment(startDate).startOf('day').toISOString(),
+        toDate: moment(endDate).endOf('day').toISOString(),
+        recordType:
+          sportFilter === 'settling'
+            ? 'Transaction'
+            : sportFilter
+            ? 'Sports'
+            : '',
+      });
+
+      const response = await getAuthData(`/users/me/transactions?${params}`);
+
+      if (response?.status === 200) {
+        setStatementData(response?.data?.data || []);
+        setPagination({
+          totalCount: response?.data?.pagination?.totalItems || 0,
         });
-        const response = await getAuthData(
-          `/user/get-all-transactions?${params}`,
-        );
-        if (response?.status === 201 || response?.status === 200) {
-          setStatementData(response?.data?.data?.statements);
-          setPagination({
-            totalCount: response?.data?.data?.count,
-          });
-        }
-      } catch (e) {
-        console.error(e);
-        return null;
       }
+    } catch (e) {
+      console.error(e);
     }
   };
+
   return (
     <div className="min-h-screen mx-1 md:mx-0 w-full">
-      <div className=" pb-2 md:py-2">
+      <div className="pb-2 md:py-2">
         <h1 className="text-18 md:text-24 font-bold mt-4 md:mt-0">
           <GradientHeading heading={'Account Statement'} />
         </h1>
       </div>
 
+      {/* DATE FILTER */}
       <div className="flex gap-[5px] items-center w-full mb-2">
         <div className="w-full">
           <p className="text-14">From Date</p>
-          <div className="relative">
-            <DatePicker
-              ref={startDatePickerRef}
-              className="px-3 text-14 font-medium py-1 w-full h-10 rounded-[4px] border border-gray-300"
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              popperPlacement="bottom-start"
-              dateFormat="dd-MM-yyyy"
-            />
-            <span
-              onClick={() => startDatePickerRef.current.setFocus()}
-              className="ay-center z-0 right-2 cursor-pointer"
-            >
-              {reactIcons.calendar}
-            </span>
-          </div>
+          <DatePicker
+            ref={startDatePickerRef}
+            className="px-3 py-1 w-full h-10 border"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="dd-MM-yyyy"
+          />
         </div>
+
         <div className="w-full">
           <p className="text-14">To Date</p>
-          <div className="relative">
-            <DatePicker
-              ref={endDatePickerRef}
-              className="px-3 text-14 font-medium py-1 w-full h-10 rounded-[4px] border border-gray-300"
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              popperPlacement="bottom-start"
-              dateFormat="dd-MM-yyyy"
-            />
-            <span
-              onClick={() => endDatePickerRef.current.setFocus()}
-              className="ay-center z-0 right-2 cursor-pointer"
-            >
-              {reactIcons.calendar}
-            </span>
-          </div>
+          <DatePicker
+            ref={endDatePickerRef}
+            className="px-3 py-1 w-full h-10 border"
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            dateFormat="dd-MM-yyyy"
+          />
         </div>
       </div>
+
+      {/* FILTER */}
       <div className="flex gap-[5px] items-center w-full mb-3">
         <div className="w-full">
           <p className="text-14">Type</p>
           <select
-            className="px-3 text-14 font-medium py-1 w-full h-10 rounded-[4px] border border-gray-300"
-            name=""
+            className="px-3 py-1 w-full h-10 border"
             value={sportFilter}
             onChange={(e) => setSportFilter(e.target.value)}
-            id=""
           >
             <option value="">All</option>
             <option value="settling">Deposit/Withdrawal</option>
-            <option value="cricket">Cricket</option>
-            <option value="soccer">Soccer</option>
-            <option value="tennis">Tennis</option>
+            <option value="Cricket">Cricket</option>
+            <option value="Soccer">Soccer</option>
+            <option value="Tennis">Tennis</option>
           </select>
-        </div>
-        <div className="w-full">
-          <div className="text-14 h-6"></div>
-          <div className="flex items-center gap-2">
-            <button className="bg-primary-1300 text-16  h-10 flex-center rounded-[4px] w-full text-white">
-              APPLY
-            </button>
-            <div className="bg-primary-1300 flex-center gap-[5px] h-[38px] w-[43px] rounded-[4px] ">
-              <img
-                src={getImage('/images/rightDrawer/downloadNew.svg')}
-                className="h-5 w-5"
-                alt=""
-              />
-            </div>
-          </div>
         </div>
       </div>
 
+      {/* TABLE */}
       <div className="data-table overflow-x-auto">
-        <table className="overflow-x-auto border-collapse whitespace-nowrap">
-          <thead className="">
-            <tr className="bg-[#8F8F8F] text-white text-14 font-bold">
+        <table className="border-collapse w-full">
+          <thead>
+            <tr className="bg-gray-500 text-white">
               <th>#</th>
               <th>Date</th>
-              <th>DESCRIPTION</th>
-              <th>CREDIT</th>
-              <th>DEBIT</th>
-              <th>BALANCE</th>
-              <th>REMARK</th>
+              <th>Description</th>
+              <th>Credit</th>
+              <th>Debit</th>
+              <th>Balance</th>
+              <th>Remark</th>
             </tr>
           </thead>
+
           <tbody>
-            {statementData && statementData?.length === 0 ? (
-              <tr className=" w-full">
-                <td colSpan={7} className="!p-0">
-                  <div className="p-1 text-center text-14 bg-[#DAE1DF] border border-[#aaa] cursor-pointer">
-                    No Event Found
-                  </div>
+            {statementData.length === 0 ? (
+              <tr>
+                <td colSpan={7} className="text-center">
+                  No Data
                 </td>
               </tr>
             ) : (
-              <>
-                {statementData?.map((_item, index) => (
-                  <tr
-                    key={index}
-                    className={`${
-                      index % 2 === 0 ? 'bg-[#E9E9E9]' : 'bg-[#F5F5F5]'
-                    } text-center`}
-                  >
-                    <td className="">{index + 1}</td>
-                    <td className=" whitespace-nowrap">
-                      {moment(_item?.updatedAt).format('DD/MM/YYYY, hh:mm')}
+              statementData.map((item, index) => {
+                const isCredit = item.type === 'Credit';
+
+                return (
+                  <tr key={index} className="text-center border">
+                    <td>{index + 1}</td>
+
+                    <td>
+                      {moment(item.timestamp).format('DD/MM/YYYY, hh:mm A')}
                     </td>
-                    <td className=" whitespace-nowrap">{_item?.remark}</td>
-                    <td className=" text-green-600">
-                      {_item.type == 'CREDIT' ||
-                      (_item.type == 'BALANCE' && !_item.amount?.includes('-'))
-                        ? numberWithCommas(_item?.amount)
-                        : 0}
+
+                    <td>{item.narration}</td>
+
+                    {/* CREDIT */}
+                    <td className="text-green-600">
+                      {isCredit ? numberWithCommas(item.amount) : 0}
                     </td>
-                    <td className=" text-red-600 ">
-                      {_item.type == 'WITHDRAW' ||
-                      (_item.type == 'BALANCE' && _item.amount?.includes('-'))
-                        ? numberWithCommas(_item?.amount)
-                        : _item.type == 'COMMISSION'
-                        ? numberWithCommas(_item?.amount)
-                        : 0}
+
+                    {/* DEBIT */}
+                    <td className="text-red-600">
+                      {!isCredit ? numberWithCommas(item.amount) : 0}
                     </td>
-                    <td className="">
-                      {_item.type == 'WITHDRAW' || _item.type == 'COMMISSION'
-                        ? numberWithCommas(_item?.senderBalance)
-                        : numberWithCommas(_item?.receiverBalance)}
-                    </td>
-                    <td className="">-</td>
+
+                    {/* BALANCE */}
+                    <td>{numberWithCommas(item.availableBalance)}</td>
+
+                    {/* REMARK */}
+                    <td>{item.context || '-'}</td>
                   </tr>
-                ))}
-              </>
+                );
+              })
             )}
           </tbody>
         </table>
       </div>
-      {statementData?.length > 0 && (
+
+      {/* PAGINATION */}
+      {statementData.length > 0 && (
         <Pagination
           pageCount={pagination.totalCount}
           setPageNumber={setPage}
